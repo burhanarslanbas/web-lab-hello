04e0b3bd837ce56b5ac525fe5cd08a5b0e654942
Burhan Arslanbaş <seninyazilimin@outlook.com>
2026-02-24 17:21:45 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div>
+  <h1>Web Tasarimi ve Programlama </h1>
+  <h2>LAB -1</h2>
+  <p>Ad Soyad: Burhanettin Arslanbaş</p>
+  <p>Ogrenci No: 220542021</p>
+</div>
+
+    </>
+  )
+}
+
+export default App
